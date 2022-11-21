b898977e85d7f57e70775d3d0961a805f5732a7e
FrancescoSanti96 <francescosanti123@gmail.com>
2022-11-21 16:20:09 +0100

    add type task and logic on input task like data and stop clear input

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -5,6 +5,7 @@ import {
   NgForm,
   Validators,
 } from '@angular/forms';
+import { task } from './task';
 
 @Component({
   selector: 'app-form',
@@ -12,8 +13,9 @@ import {
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-  tasks: string[] = [];
+  tasks: task[] = [];
   button: boolean = true;
+  inputTaskName = '';
 
   taskFormControl = new FormControl('', [Validators.required]);
   constructor() {}
@@ -21,7 +23,17 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {}
 
   onSubmit(description: string) {
-    this.tasks = this.tasks.concat(description);
+    this.tasks.push({
+      name: description,
+      dateStart: new Date().toLocaleString(),
+      dateStop: '',
+    });
     this.button = false;
+  }
+
+  onStop() {
+    this.button = true;
+    this.inputTaskName = ' ';
+    this.tasks[this.tasks.length - 1].dateStop = new Date().toLocaleString();
   }
 }
---
+++ src/app/components/form/task.ts
@@ -0,0 +1,5 @@
+export interface task {
+  name: string;
+  dateStart?: string;
+  dateStop?: string;
+}
